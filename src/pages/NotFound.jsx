fb36f476a4f7679c5cadad7d4f9efd20ba7e48ac
Dawit-y <dawityimer52@gmail.com>
2023-09-07 21:49:31 +0300

    Update routing in App.jsx

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex h-[100vh] items-center justify-center">
+      <h1 className="text-xl bold " >Not Found : 404</h1>
+    </div>
+  );
+};
+
+export default NotFound;
